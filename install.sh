5e7eabbc55c686f9e5855030f05eef192264ab4c
Nandeeka Nayak <nandeeka@gmail.com>
2024-08-07 19:54:49 -0500

    Start working on the installation

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
+
+# Install Timeloop
+cd accelergy-timeloop-infrastructure
+cd ..
+
+# Install Accelergy
+cd accelergy-library-plug-in
+pip install .
+cd ..
+cp -r custom_pc_2021 env/share/accelergy/estimation_plug_ins/accelergy-library-plugin/library
+
 jupyter lab
 deactivate
